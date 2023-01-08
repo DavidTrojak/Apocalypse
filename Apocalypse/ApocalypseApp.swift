927bd23c3e360ef6ca497adf843d3d8bd1cbba6d
Trojak David <David.Trojak@airbank.cz>
2023-01-08 12:18:29 +0100

    Initial Commit

---
+++ Apocalypse/ApocalypseApp.swift
@@ -0,0 +1,20 @@
+//
+//  ApocalypseApp.swift
+//  Apocalypse
+//
+//  Created by David Trojak on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ApocalypseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
